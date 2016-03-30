1d734e718e1cac93c24c690a5f5db7723fc99160
kochi007 <kaikobad.kaikobad326@gmail.com>
2016-03-31 01:47:07 +0600

    fourth

--- sc/seat-peaker.js
+++ sc/seat-peaker.js
@@ -1,4 +1,4 @@
-$.getJSON("json/seat-peaker.json", loadoptions);
+/*$.getJSON("json/seat-peaker.json", loadoptions);
  
  function loadoptions(data){
  	console.log(data);
@@ -10,4 +10,19 @@ $.getJSON("json/seat-peaker.json", loadoptions);
  		});
  		button.appendTo(("#button"))
  	});
- }
+ }*/
+
+/* $( ".button" ).click(function() {
+  $(this).toggleClass( "success" );
+});*/
+
+/* function changeQuestion(seat)
+	{
+document.getElementById(seat).style.backgroundColor = "#A9A9A9";
+}*/
+function myFunction() {
+    document.getElementById("seat").style.backgroundColor = "green";
+}
+function myFunction2() {
+    document.getElementById("seat2").style.backgroundColor = "green";
+}
